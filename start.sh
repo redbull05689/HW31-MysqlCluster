44548198a3433d0c287859e5d6f2f11c035465ae
Maxim Vasilev <redbull05689@gmail.com>
2019-09-17 15:06:08 +0000

    Correct all mistakes

--- start.sh
+++ start.sh
@@ -12,9 +12,9 @@ sleep 90
 
 docker-compose exec mysqlnode1 mysqlsh clusteradmin@mysqlnode1:3306 -pclusterpass -- dba create-cluster otus
 
-docker-compose exec mysqlnode1 mysqlsh clusteradmin@mysqlnode1:3306 -pclusterpass -- cluster add-instance clusteradmin@mysqlnode2 --password=clusterpass
+docker-compose exec mysqlnode1 mysqlsh clusteradmin@mysqlnode1:3306 -pclusterpass -- cluster add-instance clusteradmin@mysqlnode2 --password=clusterpass --recoveryMethod=clone
 
-docker-compose exec mysqlnode1 mysqlsh clusteradmin@mysqlnode1:3306 -pclusterpass -- cluster add-instance clusteradmin@mysqlnode3 --password=clusterpass
+docker-compose exec mysqlnode1 mysqlsh clusteradmin@mysqlnode1:3306 -pclusterpass -- cluster add-instance clusteradmin@mysqlnode3 --password=clusterpass --recoveryMethod=clone
 
 
-dckdocker-compose up -d mysqlrouter
+docker-compose up -d mysqlrouter
